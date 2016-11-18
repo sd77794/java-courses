be4ba5c5916489d82cbc50c687e6b3cfc4095ec5
sd77794 <sd77794@yandex.ru>
2016-11-18 23:06:55 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,10 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("Sum " + summ);
+		
 	}
 }
